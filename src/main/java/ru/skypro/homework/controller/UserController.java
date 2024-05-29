474c78375b6b3dcf850a4fdfabc72a6a87583054
danilbogomolov <skyprodev1769@gmail.com>
2024-05-29 15:16:29 +0300

    UserController

---
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -0,0 +1,31 @@
+package ru.skypro.homework.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.NewPassword;
+import ru.skypro.homework.dto.UpdateUser;
+import ru.skypro.homework.dto.User;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @PostMapping("/set_password")
+    public ResponseEntity<?> setPassword(@RequestBody NewPassword newPassword) {
+        return ResponseEntity.ok(new NewPassword());
+    }
+    @GetMapping("/me")
+    public ResponseEntity<?> getUser() {
+        return ResponseEntity.ok(new User());
+    }
+    @PatchMapping("/me")
+    public ResponseEntity<?> updateUser(@RequestBody UpdateUser updateUser) {
+        return ResponseEntity.ok(new UpdateUser());
+    }
+    @PatchMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> updateUserImage(@RequestParam MultipartFile image) {
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/ru/skypro/homework/dto/UpdateUser.java
@@ -0,0 +1,10 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateUser {
+    private String lastName;
+    private String phone;
+    private String firstName;
+}
