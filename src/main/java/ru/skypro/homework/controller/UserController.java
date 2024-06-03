e06fb9820746667e501f543f2d7ae9bac57583dd
YuliyaYarina <124935714+YuliyaYarina@users.noreply.github.com>
2024-06-03 15:22:45 +0500

    dto User перемещены в папку user

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -5,8 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPassword;
-import ru.skypro.homework.dto.UpdateUser;
-import ru.skypro.homework.dto.User;
+import ru.skypro.homework.dto.user.UpdateUser;
+import ru.skypro.homework.dto.user.User;
 
 @RestController
 @RequestMapping("users")
