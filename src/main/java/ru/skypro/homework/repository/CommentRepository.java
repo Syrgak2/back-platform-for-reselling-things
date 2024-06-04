3408818d384e7821d5d09a22f711781e93ba2993
Syrgak <Kary6ekov.s@gmail.com>
2024-06-04 16:52:40 +0200

    удалил модел коммент

--- src/main/java/ru/skypro/homework/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.skypro.homework.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.homework.model.CommentModel;
-
-public interface CommentRepository extends JpaRepository<CommentModel, Long> {
-
-}
