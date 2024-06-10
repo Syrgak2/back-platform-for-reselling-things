ee86ff19d080f268881afb7f22663f3b264f72ed
danilbogomolov <skyprodev1769@gmail.com>
2024-06-10 18:54:47 +0300

    mapping

--- src/main/java/ru/skypro/homework/mapper/UserMapper.java
+++ src/main/java/ru/skypro/homework/mapper/UserMapper.java
@@ -3,11 +3,13 @@ package ru.skypro.homework.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.auth.RegisterDTO;
 import ru.skypro.homework.dto.user.UpdateUserDTO;
 import ru.skypro.homework.dto.user.UserDTO;
 import ru.skypro.homework.model.User;
 @Mapper
+@Service
 public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
--- src/main/java/ru/skypro/homework/model/User.java
+++ src/main/java/ru/skypro/homework/model/User.java
@@ -2,26 +2,42 @@ package ru.skypro.homework.model;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 import ru.skypro.homework.dto.Role;
 
 import javax.persistence.*;
 import java.util.Collection;
 
+import static javax.persistence.EnumType.STRING;
+
+@Entity
 @Data
 @Getter
-@Entity
+@Setter
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
 
+    @Column(name = "username")
     private String username;
-    private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
-    private String phone;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Enumerated(STRING)
+    @Column(name = "role")
     private Role role;
 
     @OneToOne
